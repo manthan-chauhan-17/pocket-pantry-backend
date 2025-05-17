005285dc2302e6f862fcc31b71a8c894c1e4db76
Manthan Chauhan <manthanchauhan062@gmail.com>
2025-05-17 13:00:23 +0530

    --stable-- Authentication module complete

--- app.js
+++ app.js
@@ -1,12 +1,17 @@
 import express from 'express';
 import cors from 'cors';
+import healthcheckRouter from './routes/healthcheck-route.js';
+import authRouter from './routes/auth-routes.js';
 
 const app = express();
 
 // Middleware
 app.use(cors());
 app.use(express.json());
+app.use(express.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
 
-// Routes
 
-export default app;  // Export the app for testing purposes
+app.use('/api/v1', healthcheckRouter); // Register the healthcheck route
+app.use('/api/v1/auth' , authRouter); // Register the auth route
+
+export default app;
