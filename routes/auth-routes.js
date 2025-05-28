dba51f98c7b283e859e8dec2a22121acdc95b8b3
manthan-chauhan-17 <manthanchauhan062@gmail.com>
2025-05-28 11:21:24 +0530

    --stable-- change password api added

--- routes/auth-routes.js
+++ routes/auth-routes.js
@@ -1,11 +1,14 @@
 import { Router } from "express";
-import { loginUser, registerUser } from "../controllers/auth-controller.js";
+import { changePassword, loginUser, registerUser } from "../controllers/auth-controller.js";
+import verifyJwt from "../middlewares/jwt-verify.js";
 
 const authRouter = Router();
 
 authRouter.post('/register' , registerUser);
 
 authRouter.post('/login' , loginUser);
+
+authRouter.put('/change-password' , verifyJwt , changePassword) 
 
 export default authRouter;
 
